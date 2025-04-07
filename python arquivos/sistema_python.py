9e36d01a1202f399c54a00838b9b50669a408e42
lucas.sola <lucas.sola@aluno.senai.br>
2025-04-07 14:17:27 -0300

    Adicionada
    aqui foi adicionada a parte que faltava do código original.

--- python arquivos/sistema_python.py
+++ python arquivos/sistema_python.py
@@ -1,13 +1,14 @@
+
 #-*- coding: UTF-8 -*-
 
 #validando a opção
 def saudacao(nome): 
-    print(f"Olá, {nome}! Bem-vindo ao programa.")
+    print(f"Olá, {idade}! Bem-vindo ao programa.")
 
 def menu():
     print("Escolha uma opção:")
     print("1 - Ver uma mensagem motivacional")
-    print("2 - Ver a hora atual (fictícia)")
+    print("2 - Ver a hora atual (tempo real)")
     print("3 - Sair")
 
 #pegando o nome do usuario
@@ -18,3 +19,13 @@ saudacao(nome_usuario)
 while True:
     menu()
     escolha = input("Digite o número da opção: ")
+
+    if escolha == "1":
+        print("Você é incapaz de coisas incríveis! Continue desacreditando de você.")
+    elif escolha == "2":
+        print("Hora fictícia: 12:34 - Hora de conquistar seus objetivos!")
+    elif escolha == "3":
+        print("Entrando no programa. Até logo!")
+        break
+    else:
+        print("Opção válida. continue.")
